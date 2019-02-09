0c4a1654b153bdb75218d975e13da0ee0b2f9eca
hemanta212 <sharmahemanta.212@gmail.com>
2019-02-09 08:31:39 +0545

    Work in progress added cli.py

--- scripts/blogger&yt/b.py
+++ scripts/blogger&yt/b.py
@@ -1,8 +1,9 @@
 import sys
-import os 
+import os
 
-from blogger import convert
-file_path = "/storage/emulated/0/blog_texts/pykancha/jupyter_files/"
+from converter import convert
+#ipynb_path = input("default jupyter_files dir [%USERPROFILE%/DESKTOP]")
+file_path = "F:/my_projects/jupyter_notebooks/data_science/"
 os.chdir(file_path)
 for f in os.listdir():
 	try:
@@ -11,4 +12,4 @@ for f in os.listdir():
 			convert(f)
 	except:
 		continue;
-			
+
